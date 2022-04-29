46aaeef64917ba732c4877039afa6907de7b569b
PauloVOFonseca <paulovitoradsifba@gmai.com>
2022-04-29 18:47:46 -0300

    criação do botão de pausa e dispose

---
+++ lib/game/utils/pause_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:game/game/game_maker.dart';
+import 'package:game/game/utils/pause_menu.dart';
+
+class PauseWidget extends StatelessWidget {
+  static const String id = 'PauseButton';
+  final GameMaker gameRef;
+
+  const PauseWidget({Key? key, required this.gameRef}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: GestureDetector(
+        onTap: () {
+          gameRef.pauseEngine();
+          gameRef.overlays.add(PauseMenu.id);
+          gameRef.overlays.remove(PauseWidget.id);
+        },
+        child: const Icon(
+          Icons.pause_circle_outline_outlined,
+          color: Colors.white,
+          size: 30,
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:game/game/game_maker.dart';
+import 'package:game/game/utils/pause_menu.dart';
+import 'package:game/game/utils/pause_widget.dart';
 
 class GameScreen extends StatefulWidget {
   //maingamestate
@@ -14,9 +16,22 @@ class _GameScreenState extends State<GameScreen> {
   GameMaker game = GameMaker();
 
   @override
+  void dispose() {
+    game.removeFromParent();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GameWidget(game: game),
+      body: GameWidget(
+        game: game,
+        initialActiveOverlays: const [PauseWidget.id],
+        overlayBuilderMap: {
+          PauseWidget.id: (BuildContext context, GameMaker gameRef) => PauseWidget(gameRef: gameRef),
+          PauseMenu.id: (BuildContext context, GameMaker gameRef) => PauseMenu(gameRef: gameRef),
+        },
+      ),
     );
   }
 }
