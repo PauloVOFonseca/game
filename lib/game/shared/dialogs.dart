008b814ed8fd1207a1604335168c279b1231a1c7
PauloVOFonseca <paulovitoradsifba@gmai.com>
2022-05-11 21:21:51 -0300

    mais dialogs

--- lib/game/shared/dialogs.dart
+++ lib/game/shared/dialogs.dart
@@ -1,12 +1,46 @@
 class Dialogs {
   Map dialogs = {
-    'greetings':
+    "greetings":
         'Olá, aqui você irá aprender o básico do jogo e suas informações. Use o Joystick, para se movimentar e pegue as folhas de papel espalhadas no mapa.',
-    'howToMove':
+    "howToMove":
         'Para derrotar um inimigo, basta pular sobre eles. Mas cuidado, não encoste na sua lateral ou você perderá e voltará do começo.',
-    'energyScore':
+    "energyScore":
         'Toda fase terá diversas folhas, elas possuem informações importante sobre a Obesidade e podem ser revistas no menu do jogo.',
-    'boss':
-        'Para completar a fase, você precisa derrotar o chefão do estágio, pule sobre ele para finaliza-lo.',
+    "boss":
+        "Para completar a fase, você precisa derrotar o chefão do estágio, pule sobre ele para finaliza-lo.",
+    "firstTip":
+        "Ainda bem que você chegou! Precisamos da sua ajuda. Colete as informações disponíveis e combater os inimigos para conseguir nos ajudar.",
+    "definition":
+        "A obesidade é uma doença crônica, definida pela Organização Mundial de Saúde (OMS) como o acúmulo excessivo de gordura no corpo.",
+    "imc":
+        "Segundo o IMC, uma pessoa é considerada obesa caso seu índice seja igual ou superior a 30. Para calcular basta dividir o peso pelo quadrado da altura.",
+    "cause":
+        "A obesidade aparece quando a energia ingerida com os alimentos supera muito a quantidade de energia gasta nas atividades diárias. Outros fatores também interferem, como a genética, a situação socioeconômica e o ambiente em que vivemos.",
+    "combatThis":
+        "A melhor forma de combater a obesidade é manter uma dieta balanceada e praticar exercícios físicos.",
+    "unbalancedDiet":
+        "Uma dieta desbalanceada pode causar muitos problemas de saúde, interferindo além da obesidade.",
+    "metabolicSyndrome":
+        "A síndrome metabólica é um conjunto de fatores de riscos manisfestados num indivíduo e está associada a obesidade, identificada pela grande quantidade de gordura abdominal, elevação da taxa de triglicerídeos e glicose, além da pressão alta.",
+    "whatAffects":
+        "A obesidade afeta principalmente: o sistema cardiovascular, o sistema respiratório e até mesmo articulações.",
+    "cardiovascularSystem":
+        "Como o sistema cardiovascular é afetado: aumento da pressão arterial, entupimento de veias. Dificultando o funcionamento do coração e podendo causar Hipertensão.",
+    "respiratorySystem":
+        "Como o sistema respiratório é afetado: falta de ar, geração da apneia do sono e intensificação de efeitos da asma.",
+    "skeletalSystem":
+        "Como o sistema esqueletico é afetado: O excesso de peso força as articulações (coluna, bacia, joelhos, calcanhares). Pode gerar hérnia de disco e sensibilidade nas articulações.",
+    "moreConsequences":
+        "A obesidade também pode causar cânceres de mama, cólon e útero, além de problemas psicológicos",
+    "diet":
+        "Cada dieta deve ser adaptada para o estilo de vida pessoal, sob supervisão de um profissional especializado.",
+    "adultsIBGE":
+        "Em 2020, foi anunciado pelo IBGE que 1 em cada 4 adultos estão obesos(cerca de 25% da população).",
+    "childrenIBGE":
+        "A obesidade infantil afeta pelo menos 3 milhões de crianças menores de 10 anos no Brasil, afetando 13,2% das crianças.",
+    "costs":
+        "O tratamento de doenças relacionadas à obesidade e sobrepeso custam ao Estado cerca de 3,7 bilhões por ano.",
+    "worldDay":
+        "11/10 é o Dia Mundial da Obesidade e Dia Nacional de Prevenção da Obesidade.",
   };
 }
